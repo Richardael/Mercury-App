d91682c9eb18db5b849acf9cabec8380c30a94f9
Richardael <richardael14@gmail.com>
2024-10-28 13:05:23 -0400

    Estructura de Color
    Primera subida

---
+++ mercury-app/src/App.tsx
@@ -0,0 +1,96 @@
+import './App.css'
+
+function App() {
+
+
+  return (
+    <>
+    {/* Dasboard */}
+    <div className='bg-red-500 h-screen'>
+          {/* Header */}
+    <div className='bg-blue-500 p-4'>
+      <h1>Mercury</h1>
+    </div>
+    {/* Content */}
+    <div className='bg-green-500 p-4 grid grid-cols-8'>
+    {/* Lista */}
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    <div className='col-span-1'>
+      <h2>Lista</h2>
+      <p>Here you can see the list of your employees.</p>
+      {/* Carta */}
+      <div>
+        <h3>Carta</h3>
+        <p>Here you can see the information of an employee.</p>
+      </div>
+    </div>
+    </div>
+    </div>
+
+    </>
+  )
+}
+
+export default App
